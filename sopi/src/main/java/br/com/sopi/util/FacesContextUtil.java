cc97e0c38ff500f5824fc05272812fd301ccd612
Universidade Federal <Universidade Federal@192.168.0.105>
2014-08-15 18:27:13 -0300

    reiniciando o projeto

---
+++ sopi/src/main/java/br/com/sopi/util/FacesContextUtil.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.sopi.util;
+
+/**
+ *
+ * @author Universidade Federal
+ */
+import javax.faces.context.FacesContext;
+import org.hibernate.Session;
+
+public class FacesContextUtil {
+
+    private static final String HIBERNATE_SESSION = "hibernate_session";
+
+    public static void setRequestSession(Session session){
+        FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put(HIBERNATE_SESSION, session);
+    }
+    
+    public static Session getRequestSession() {
+        return (Session)FacesContext.getCurrentInstance().getExternalContext().getRequestMap().get(HIBERNATE_SESSION);
+    }
+    
+}
